73779ad98350c57270063c0f569a97e94a8e6092
bdurushia <b.durushia@gmail.com>
2021-12-03 20:55:02 -0600

    Testing installation of inquirer package to use in app

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log("Portfolio HTML File created succesfully. Check the index.html to see the output!");
+// });
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log("Portfolio HTML File created succesfully. Check the index.html to see the output!");
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
